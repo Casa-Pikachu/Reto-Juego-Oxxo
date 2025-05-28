f57fbd76f7e1ded0d1d25b92b86c0e29363072bc
Bruno Zabala <brunozabalap@gmail.com>
2025-05-28 14:24:04 -0600

    Minijuego caja - avance

---
+++ Assets/MainMenu y Minijuego1/Scripts/CashierSFX.cs
@@ -0,0 +1,39 @@
+using UnityEditor.Search;
+using UnityEngine;
+
+public class CashierSFX : MonoBehaviour
+{
+    public AudioClip scanner;
+    public AudioClip completarCompra;
+    public AudioClip error;
+    public AudioClip correcto;
+    [SerializeField] AudioSource audioSource;
+
+    public void PlayScanner()
+    {
+        Debug.Log("Scanner");
+        audioSource.clip = scanner;
+        audioSource.Play();
+    }
+
+    public void PlayCompletarCompra()
+    {
+        Debug.Log("Completar Compra");
+        audioSource.clip = completarCompra;
+        audioSource.Play();
+    }
+
+    public void PlayError()
+    {
+        Debug.Log("Error");
+        audioSource.clip = error;
+        audioSource.Play();
+    }
+    
+    public void PlayCorrecto()
+    {
+        Debug.Log("Correcto");
+        audioSource.clip = correcto;
+        audioSource.Play();
+    }
+}
