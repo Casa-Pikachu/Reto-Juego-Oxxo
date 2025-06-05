7badc1d4e0af8599ce9f1a74a14a6ca4041bfe72
nina-col <giannina@colunga.net>
2025-06-04 22:21:22 -0600

    ya se agregaron los sprites para el progreso de la tienda
    ya se agregaron los sprites para el progreso de la tienda

---
+++ Assets/MainMenu y Minijuego1/Scripts/NivelesController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class NivelesController : MonoBehaviour
+{
+    //referencias, para hacer algo asi
+    // https://www.youtube.com/watch?v=hj9ikydHttk 
+    // https://www.youtube.com/watch?v=RgQGHmdY3mE
+
+    //ya se agregaron los sprites para el progreso de la tienda 
+    //Nivel0_0
+    //Nivel1_0
+    //Nivel2_0
+    //Nivel3_0
+    //Nivel4_0
+    //Nivel5_0
+
+    //el progreso activa cada nivel, no desactiva los demas porque si no ya no hay fondo 
+
+
+    //si hay tiempo barra de progreso si no pues ocn puros puntos
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
